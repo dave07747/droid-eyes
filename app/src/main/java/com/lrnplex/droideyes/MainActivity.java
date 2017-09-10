47067199ad4d0dd97e7aa1cf5ec8b162b5051c44
David Kluszczynski <dave07747@gmail.com>
2017-09-10 06:18:21 -0400

    Individual works

--- app/src/main/java/com/lrnplex/droideyes/MainActivity.java
+++ app/src/main/java/com/lrnplex/droideyes/MainActivity.java
@@ -1,17 +1,22 @@
 package com.lrnplex.droideyes;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -31,8 +36,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/*import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
@@ -43,12 +57,26 @@ import com.google.api.services.vision.v1.model.AnnotateImageRequest;
 import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
 import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
 import com.google.api.services.vision.v1.model.EntityAnnotation;
-import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Feature;*/
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
 import com.google.api.services.vision.v1.model.Image;
 
 import static android.content.ContentValues.TAG;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 
 public class MainActivity extends Activity implements OnInitListener {
 
@@ -98,8 +126,10 @@ public class MainActivity extends Activity implements OnInitListener {
 
         MyTimerTask myTask = new MyTimerTask();
         Timer myTimer = new Timer();
+        myTimer.schedule(myTask, 3000, 1500);
 
-        myTimer.schedule(myTask, 100, 100);
+
+
 
 
     }
@@ -141,6 +171,7 @@ public class MainActivity extends Activity implements OnInitListener {
                 // Call startPreview before taking a picture
                 mCamera.startPreview();
                 mCamera.takePicture(null, null, null, mPictureCallback);
+
                 //file = new File(getFilesDir(), fileName);
             } catch (NullPointerException ne) {
                 ne.printStackTrace();
@@ -177,123 +208,154 @@ public class MainActivity extends Activity implements OnInitListener {
     };
 
 
-    private void callCloudVision(final byte[] imageBytes) throws IOException {
-        // Do the real work in an async task, because we need to use the network anyway
+    private void callCloudVision(final byte[] data) throws JSONException {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://vision.googleapis.com/v1/images:annotate?key=" + CLOUD_VISION_API_KEY;
+
+        JSONObject json1= new JSONObject();
+        json1.put("type", "LABEL_DETECTION");
+        json1.put("maxResults", 1);
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(json1);
+
+        JSONObject json3= new JSONObject();
+        Image base64EncodedImage = new Image();
+        // Convert the bitmap to a JPEG
+        // Just in case it's a format that Android understands but Cloud Vision
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inPreferredConfig = Bitmap.Config.RGB_565;
+        Log.d("callCloudVision", String.valueOf(data.length));
+        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length, opt);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+        // Base64 encode the JPEG
+        base64EncodedImage.encodeContent(imageBytes);
+        json3.put("content", String.valueOf(base64EncodedImage).substring(9, String.valueOf(base64EncodedImage).length()-1 ));
+
+        JSONObject json2= new JSONObject();
+        json2.put("features", jsonArray);
+        json2.put("image", json3);
+
+        JSONArray jsonArray2 = new JSONArray();
+        jsonArray2.put(json2);
+
+        JSONObject jsonFinal= new JSONObject();
+        jsonFinal.put("requests", jsonArray2);
+
+        Log.d("jsonFinal", String.valueOf(jsonFinal));
+
+        String response = POST(url, jsonFinal);
+
+        Log.d("Final Response Cloud", response);
+
+        // Request a string response from the provided URL.
+       /* JsonObjectRequest request_json = new JsonObjectRequest(url, jsonFinal,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //Process os success response
+
+                        Log.d("Cloud Response", String.valueOf(response));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error);
+                Log.d("Cloud error", String.valueOf(error));
+            }
+        });
+
+        queue.add(request_json);*/
+
+    }
+
+    private String POST(final String url, final JSONObject jsonObject) {
         new AsyncTask<Object, Void, String>() {
             @Override
             protected String doInBackground(Object... params) {
+                InputStream inputStream = null;
+                String result = "";
                 try {
-                    Log.d(TAG, "callCloudVision: *************************0");
 
-                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
-                    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+                    // 1. create HttpClient
+                    HttpClient httpclient = new DefaultHttpClient();
+                    Log.d("Errrrrr", "1");
 
-                    VisionRequestInitializer requestInitializer =
-                            new VisionRequestInitializer(CLOUD_VISION_API_KEY) {
-                                /**
-                                 * We override this so we can inject important identifying fields into the HTTP
-                                 * headers. This enables use of a restricted cloud platform API key.
-                                 */
-                                @Override
-                                protected void initializeVisionRequest(VisionRequest<?> visionRequest)
-                                        throws IOException {
-                                    super.initializeVisionRequest(visionRequest);
+                    // 2. make POST request to the given URL
+                    HttpPost httpPost = new HttpPost(url);
+                    Log.d("Errrrrr", "2");
+                    String json;
 
-                                    String packageName = getPackageName();
-                                    visionRequest.getRequestHeaders().set(ANDROID_PACKAGE_HEADER, packageName);
+                    // 3. build jsonObject
 
-                                    String sig = PackageManagerUtils.getSignature(getPackageManager(), packageName);
+                    // 4. convert JSONObject to JSON to String
+                    json = jsonObject.toString();
+                    Log.d("Errrrrr", "4");
 
-                                    visionRequest.getRequestHeaders().set(ANDROID_CERT_HEADER, sig);
-                                }
-                            };
-                    Log.d(TAG, "callCloudVision: *************************1");
+                    // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+                    // ObjectMapper mapper = new ObjectMapper();
+                    // json = mapper.writeValueAsString(person);
 
-                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
-                    builder.setVisionRequestInitializer(requestInitializer);
-                   /* builder.setApplicationName(CLOUD_VISION_APPLICATION_NAME);*/
+                    // 5. set json to StringEntity
+                    StringEntity se = new StringEntity(json);
+                    Log.d("Errrrrr", "5");
 
-                    Vision vision = builder.build();
+                    // 6. set httpPost Entity
+                    httpPost.setEntity(se);
+                    Log.d("Errrrrr", "6");
 
-                    BatchAnnotateImagesRequest batchAnnotateImagesRequest =
-                            new BatchAnnotateImagesRequest();
-                    batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
-                        AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+                    // 7. Set some headers to inform server about the type of the content
+                    httpPost.setHeader("Accept", "application/json");
+                    httpPost.setHeader("Content-type", "application/json");
+                    Log.d("Errrrrr", "7");
 
-                        // Add the image
-                        Image base64EncodedImage = new Image();
-                        // Convert the bitmap to a JPEG
-                        // Just in case it's a format that Android understands but Cloud Vision
-                        BitmapFactory.Options opt = new BitmapFactory.Options();
-                        opt.inPreferredConfig = Bitmap.Config.RGB_565;
-                        Log.d("callCloudVision", String.valueOf(imageBytes.length));
-                        Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length, opt);
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
-                        bitmap.recycle();
-                        byte[] imageByte = byteArrayOutputStream.toByteArray();
+                    // 8. Execute POST request to the given URL
+                    HttpResponse httpResponse = httpclient.execute(httpPost);
+                    Log.d("Errrrrr", "8");
 
-                        // Base64 encode the JPEG
-                        base64EncodedImage.encodeContent(imageByte);
-                        annotateImageRequest.setImage(base64EncodedImage);
+                    // 9. receive response as inputStream
+                    inputStream = httpResponse.getEntity().getContent();
+                    Log.d("Errrrrr", "9");
 
-                        // add the features we want
-                        annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
-                            Feature labelDetection = new Feature();
-                            labelDetection.setType("LABEL_DETECTION");
-                            labelDetection.setMaxResults(1);
-                            add(labelDetection);
-                        }});
+                    // 10. convert inputstream to string
+                    if (inputStream != null)
+                        result = convertInputStreamToString(inputStream);
+                    else
+                        result = "Did not work!";
 
-                        // Add the list of one thing to the request
-                        add(annotateImageRequest);
-                    }});
+                    Log.d("Errrrrr", "10 - Finished");
 
-                    Vision.Images.Annotate annotateRequest =
-                            vision.images().annotate(batchAnnotateImagesRequest);
-                    // Due to a bug: requests to Vision API containing large images fail when GZipped.
-                    annotateRequest.setDisableGZipContent(true);
-                    Log.d("callCloudVision", "created Cloud Vision request object, sending request");
-
-                    BatchAnnotateImagesResponse response = annotateRequest.execute();
-                    Log.d("callCloudVision", "Response received");
-                    return convertResponseToString(response);
-
-                } catch (GoogleJsonResponseException e) {
-                    Log.d(TAG, "failed to make API request because " + e.getContent());
-                } catch (IOException e) {
-                    Log.d(TAG, "failed to make API request because of other IOException " +
-                            e.getMessage());
+                } catch (Exception e) {
+                    Log.d("InputStream", e.getMessage());
                 }
-                return "Cloud Vision API request failed. Check logs for details.";
-            }
 
+                // 11. return result
+                Log.d("Finish",result);
+                speakWords(result);
+                return result;
+            }
             protected void onPostExecute(String result) {
                 //mImageDetails.setText(result);
                 Log.d("callCloudVision", "onPostExecute: " + result);
-                speakWords(result);
+
+                //speakWords(result);
             }
         }.execute();
+        return "...";
     }
 
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
 
+        inputStream.close();
+        return result;
 
-    private String convertResponseToString(BatchAnnotateImagesResponse response) {
-        String message = ""; // = "I found these things:\n\n";
-
-        List<EntityAnnotation> labels = response.getResponses().get(0).getLabelAnnotations();
-        if (labels != null) {
-            for (EntityAnnotation label : labels) {
-                message += label.getDescription(); //String.format(Locale.US, "%.3f: %s", label.getScore(), label.getDescription());
-                //message += "\n";
-            }
-        } else {
-            message += "nothing";
-        }
-
-
-
-        return message;
     }
 
     @Override
